ba4e1ae9126fe61c46b4740957b946fb36482ec9
2br-2b <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2020-09-16 14:44:10 -0400

    Reformatted code

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
-if(!localStorage.status) localStorage['status'] = 1;
-if(localStorage.status == 1){
+if (!localStorage.status) localStorage['status'] = 1;
+if (localStorage.status == 1) {
     $("#anchor--toggle input").prop("checked", true);
     $("#anchor--toggle label span").text("on");
 } else {
     $("#anchor--toggle input").prop("checked", false);
     $("#anchor--toggle label span").text("off");
 }
-$("#anchor--toggle").mousedown(function(){
-    if($("#anchor--input").is(":checked")){
+$("#anchor--toggle").mousedown(function () {
+    if ($("#anchor--input").is(":checked")) {
         localStorage.status = 0;
         $("#anchor--toggle label span").text("off");
         chrome.tabs.reload();
@@ -18,14 +18,14 @@ $("#anchor--toggle").mousedown(function(){
     }
 });
 
-$('body').on('click', 'a', function(){
+$('body').on('click', 'a', function () {
     chrome.tabs.create({url: $(this).attr('href')});
     return false;
 });
 
-$("#anchor--info label").mousedown(function(){
+$("#anchor--info label").mousedown(function () {
     $("#anchor--info label").addClass("button--clicked");
-    setTimeout(function(){
+    setTimeout(function () {
         $("#anchor--info label").removeClass("button--clicked");
     }, 100);
     $("body").toggleClass("info--open");
